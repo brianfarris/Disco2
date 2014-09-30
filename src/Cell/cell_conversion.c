e5ce965b0761d0518125ed8b930f462f273a5e20
Brian Farris <briandavidfarris@gmail.com>
2014-09-30 16:00:48 -0400

    still seems to work with vortex

--- src/Cell/cell_conversion.c
+++ src/Cell/cell_conversion.c
@@ -10,7 +10,6 @@
 
 void cell_prim2cons( double * prim , double * cons , double r , double dV ,struct Sim * theSim){
   double GAMMALAW = sim_GAMMALAW(theSim);
-  int runtype = sim_runtype(theSim);
 
   double rho = prim[RHO];
   double Pp  = prim[PPP];
@@ -56,7 +55,6 @@ void cell_calc_cons( struct Cell *** theCells,struct Sim *theSim ){
 }
 
 void cell_cons2prim( double * cons , double * prim , double r , double dV ,struct Sim * theSim){
-  int runtype = sim_runtype(theSim);
   double CS_FLOOR = sim_CS_FLOOR(theSim);
   double CS_CAP = sim_CS_CAP(theSim);
   double RHO_FLOOR = sim_RHO_FLOOR(theSim);
