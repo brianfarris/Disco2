bc9c391032dbdcc59cfb6fe4aa8b47516b2ef3b8
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2014-12-15 01:55:41 -0500

    eos_cool works with GRDisc.

--- src/EOS/eos_cool.c
+++ src/EOS/eos_cool.c
@@ -23,13 +23,23 @@ double eos_cool_isotherm(double *prim, double H, struct Sim *theSim)
 double eos_cool_bb_es(double *prim, double H, struct Sim *theSim)
 {
     //TODO: Work in 2D and 3D?
+    double rho, T;
+    if(sim_Background(theSim) != GRDISC)
+    {
+        double P;
+        rho = (prim[RHO]/H)*eos_rho_scale;
+        P = (prim[PPP]/H)*eos_rho_scale;
+        T = 0.5 * P / rho * eos_mp*eos_c*eos_c; //in ergs
+    }
+    else
+    {
+        rho = prim[RHO]*eos_rho_scale;
+        T = 0.5 * prim[TTT] * eos_mp*eos_c*eos_c; //in ergs
+    }
     double kappa = 0.2;
     double h = H*eos_r_scale;
-    double rho = (prim[RHO]/H)*eos_rho_scale;
-    double P = (prim[PPP]/H)*eos_rho_scale;
-    double temp = 0.5 * eos_mp * P / rho * eos_c * eos_c;
     double q0 = sim_CoolPar1(theSim);
-    double q = q0 * 8.0*eos_sb * temp*temp*temp*temp/(3.0*kappa*rho*h);
+    double q = q0 * 8.0*eos_sb * T*T*T*T/(3.0*kappa*rho*h);
     double Q = q / (eos_c*eos_c*eos_c * eos_rho_scale);
 
     return Q;
@@ -38,13 +48,23 @@ double eos_cool_bb_es(double *prim, double H, struct Sim *theSim)
 double eos_cool_bb_ff(double *prim, double H, struct Sim *theSim)
 {
     //TODO: Work in 2D and 3D?
+    double rho, T;
+    if(sim_Background(theSim) != GRDISC)
+    {
+        double P;
+        rho = (prim[RHO]/H)*eos_rho_scale;
+        P = (prim[PPP]/H)*eos_rho_scale;
+        T = 0.5 * P / rho * eos_mp*eos_c*eos_c; //in ergs
+    }
+    else
+    {
+        rho = prim[RHO]*eos_rho_scale;
+        T = 0.5 * prim[TTT] * eos_mp*eos_c*eos_c; //in ergs
+    }
+    double kappa = 6.6e22 * rho * pow(T/eos_k, -3.5);
     double h = H*eos_r_scale;
-    double rho = (prim[RHO]/H)*eos_rho_scale;
-    double P = (prim[PPP]/H)*eos_rho_scale;
-    double temp = 0.5 * eos_mp * P / rho * eos_c * eos_c; //in ergs
-    double kappa = 6.6e22 * rho * pow(temp/eos_k, -3.5);
     double q0 = sim_CoolPar1(theSim);
-    double q = q0 * 8.0*eos_sb * temp*temp*temp*temp/(3.0*kappa*rho*h);
+    double q = q0 * 8.0*eos_sb * T*T*T*T/(3.0*kappa*rho*h);
     double Q = q / (eos_c*eos_c*eos_c * eos_rho_scale);
 
     return Q;
@@ -52,10 +72,20 @@ double eos_cool_bb_ff(double *prim, double H, struct Sim *theSim)
 
 double eos_cool_neutrino(double *prim, double H, struct Sim *theSim)
 {
-    double rho = (prim[RHO]/H)*eos_rho_scale;
-    double P = (prim[PPP]/H)*eos_rho_scale;
+    double rho, t11;
+    if(sim_Background(theSim) != GRDISC)
+    {
+        double P;
+        rho = (prim[RHO]/H)*eos_rho_scale;
+        P = (prim[PPP]/H)*eos_rho_scale;
+        t11 = 0.5 * P / rho * eos_mp*eos_c*eos_c/(1.0e11*eos_k);
+    }
+    else
+    {
+        rho = prim[RHO] * eos_rho_scale;
+        t11 = 0.5 * prim[TTT] * eos_mp*eos_c*eos_c/(1.0e11*eos_k);
+    }
     double h = H * eos_r_scale;
-    double t11 = 0.5 * eos_mp * P / rho * eos_c * eos_c / (1.0e11 * eos_k);
     double q = 5.0e33 * pow(t11,9) * h;
     double Q = q / (eos_c*eos_c*eos_c * eos_rho_scale);
 
