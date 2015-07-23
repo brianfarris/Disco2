b95dd46292388b1dfe3c78704b403f2d008940ad
Daniel John D'Orazio <ddorazio@pfe24.nas.nasa.gov>
2015-07-23 15:16:55 -0700

    changed size of diag array to hold extra ecc calc

--- src/Diagnostics/diagnostics_create_destroy.c
+++ src/Diagnostics/diagnostics_create_destroy.c
@@ -11,7 +11,7 @@
 struct Diagnostics *diagnostics_create(struct Sim * theSim, struct TimeStep * theTimeStep, struct MPIsetup * theMPIsetup) {
   struct Diagnostics * theDiagnostics = (struct Diagnostics *) malloc(sizeof(struct Diagnostics));
 
-  theDiagnostics->NUM_DIAG = 22;
+  theDiagnostics->NUM_DIAG = 28;
 
 
   int * N_p_global_temp = malloc(sizeof(int)*sim_N_global(theSim,R_DIR));
